3ca3f09031adf58784475d79fdd7bc3692ca0340
Tran <qtran31@dxc.com>
2020-04-02 12:53:48 +0100

    readd component

---
+++ src/Components/Shared/CardBlog/index.tsx
@@ -0,0 +1,40 @@
+import React, { FunctionComponent } from "react";
+import { Badget, Color } from "../Badget";
+import './CardBlog.css';
+
+type CardBlogProps = {
+  comment: string;
+  color: Color;
+  date: string;
+  description: string;
+  header: string;
+  share: string;
+  title: string;
+};
+
+export const CardBlog: FunctionComponent<CardBlogProps> = ({comment, color, date, description, header, share, title}) => {
+  return (
+    <div className="ui link cards">
+    <div className="card">
+      <div className="image">
+      <img />
+        {/* <img src="/images/avatar2/large/matthew.png" /> */}
+        <Badget title={title} color={color}/>
+      </div>
+      <div className="content">
+        <div className="meta">
+          <a>{date}</a>
+          <a>{comment}</a>
+          <a>{share}</a>
+        </div>
+        <div className="header">{header}</div>
+        <div className="description">{description}</div>
+      </div>
+      <div className="extra content">
+        Read more
+        <i className="arrow right icon"></i>
+      </div>
+    </div>
+    </div>
+  );
+};
---
+++ src/Shared/Badget/index.tsx
@@ -0,0 +1,26 @@
+import React, { FunctionComponent } from "react";
+import { Label } from "semantic-ui-react";
+
+export enum Color {
+  Red = "red",
+  Orange = "orange",
+  Blue = "blue"
+}
+
+type BadgetProps = {
+  title: string;
+  color: Color;
+};
+
+const badgetStyle = {
+  borderRadius: "30px"
+};
+
+export const Badget: FunctionComponent<BadgetProps> = ({ title, color }) => (
+  <div>
+    <Label style={badgetStyle} color={color}>
+      {title}
+      {color}
+    </Label>
+  </div>
+);
