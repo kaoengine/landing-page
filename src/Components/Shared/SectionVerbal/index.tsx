03fe063f1a0f8682dc651f7f711539ad1efa10dc
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-04-08 15:17:42 +0700

    Quan-SectionVerbalComponent-revised-optional-type

--- src/Components/Shared/SectionVerbal/index.tsx
+++ src/Components/Shared/SectionVerbal/index.tsx
@@ -1,18 +1,20 @@
 import React, { FunctionComponent } from "react";
 
-
 type BadgetProps = {
   header: string;
-  subHeader: string;
+  subHeader?: string;
 };
 
-const SectionVerbal: FunctionComponent<BadgetProps> = ({ header, subHeader }) => {
+const SectionVerbal: FunctionComponent<BadgetProps> = ({
+  header,
+  subHeader
+}) => {
   return (
     <h2 className="ui header">
       {header}
-     <div className="sub header">{subHeader}</div>
-  </h2>
-	)
+      <div className="sub header">{subHeader}</div>
+    </h2>
+  );
 };
 
 export default SectionVerbal;
