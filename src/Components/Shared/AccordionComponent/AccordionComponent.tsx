83ff9551078005db9446f17b570c8368cc38c0b9
Khanh <tran_khanh@outlook.com>
2020-04-09 15:38:37 +0700

    Acco update

--- src/Components/Shared/AccordionComponent/AccordionComponent.tsx
+++ src/Components/Shared/AccordionComponent/AccordionComponent.tsx
@@ -1,32 +1,39 @@
 import * as React from "react";
 
 import faker from "faker";
-import _ from "lodash";
+import times from "lodash/times";
 import { Accordion } from "semantic-ui-react";
 
 type IState = {
   activeIndex: boolean;
 };
-const panels = _.times(1, i => ({
+
+const panels = times(1, i => ({
   key: `panel-${i}`,
   title: faker.lorem.sentence(),
   content: faker.lorem.paragraphs()
 }));
 
 export default class AccordionComponent extends React.Component<IState> {
-  state: IState = { activeIndex: true };
 
-  public handleTitleClick = (val: boolean) => {
-    this.setState({ activeIndex: !val });
+  private _handleTitleClick = (e: Event, data: any) => {
+    e.preventDefault();
+    const { active } = data;
+    console.info('active', active);
+
   };
+
   public render() {
-    const { activeIndex } = this.state;
     return (
       <div>
         <Accordion
           value={this.props.children}
           panels={panels}
-          onClick={() => this.handleTitleClick(activeIndex)}
+
+          onTitleClick={(e: any, data) => {
+            this._handleTitleClick(e, data);
+          }}
+
         />
       </div>
     );
