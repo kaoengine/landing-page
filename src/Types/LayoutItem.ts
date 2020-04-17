e07c1fa2f0953cf2a673b52821e8e7b4441f398b
Tran <qtran31@dxc.com>
2020-04-17 15:26:30 +0100

    U Type

--- src/Types/LayoutItem.ts
+++ src/Types/LayoutItem.ts
@@ -1,15 +1,15 @@
 import { ReactElement } from "react";
 
 export enum LayoutItemType {
-  Nav = "nav",
-  Section = "sec",
-  sectionAbout = "secAbout",
-  sectionFeature = "secFeature",
-  sectionPricing = "secPricing",
-  sectionLatestNews = "secLatestNews",
-  Form = "form",
-  Footer = "foo",
-  Modal = "mod",
+  Nav = "Nav",
+  Section = "Section",
+  SectionAbout = "SectionAbout",
+  SectionFeature = "SectionFeature",
+  SectionPricing = "SectionPricing",
+  SectionLatestNews = "SecLatestNews",
+  Form = "Form",
+  Footer = "Footer",
+  Modal = "Modal",
 }
 
 export type KaoComponent = {
