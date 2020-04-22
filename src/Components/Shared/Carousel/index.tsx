2617751bb4796cd818f3ba51ead71f56038cbad1
Khanh <tran_khanh@outlook.com>
2020-04-22 20:27:05 +0700

    [implement] Carousel

--- src/Components/Shared/Carousel/index.tsx
+++ src/Components/Shared/Carousel/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, Image, DotGroup } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-
+import styled from "styled-components";
 
 const photos = [
   {
@@ -20,23 +20,75 @@ const photos = [
     name: "photo4",
     url: "http://appco.themetags.com/img/04.png",
   },
+  {
+    name: "photo4",
+    url: "http://appco.themetags.com/img/04.png",
+  },
+  {
+    name: "photo4",
+    url: "http://appco.themetags.com/img/04.png",
+  },
 ];
 
+const Wrapper = styled.div`
+   background:#6730e3;
+   padding:5%;
+`;
+
+const PhoneFrame = styled.div`
+    background-image: url(http://appco.themetags.com/img/phone-frame2.png);
+    background-repeat: no-repeat;
+    background-size: 100% 100%;
+    display: block;
+    width: 285px;
+    height: 518px;
+    position: absolute;
+    top: 60px;
+    left: calc(50% + 0.5px);
+    -webkit-transform: translateX(-50%);
+    transform: translateX(-50%);
+    z-index: 2;
+    border-radius: 40px;`;
+
+
+const ImageWrapper = styled.div`
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 64%;
+    height: 100%
+`;
+
+const DotWrapper = styled.div`
+  text-align: center;
+  margin-top:4em;
+`;
 class Carousel extends React.Component {
   public render() {
     return (
-      <CarouselProvider
-        naturalSlideWidth={100}
-        naturalSlideHeight={125}
-        totalSlides={3}
-      >
-        <Slider>
-          <Slide index={0}>I am the first Slide.</Slide>
-          <Slide index={1}>I am the second Slide.</Slide>
-          <Slide index={2}>I am the third Slide.</Slide>
-        </Slider>
-      </CarouselProvider>
-    );
+      <Wrapper>
+        <PhoneFrame></PhoneFrame>
+        <CarouselProvider
+          naturalSlideWidth={100}
+          naturalSlideHeight={125}
+          totalSlides={5}
+          hasMasterSpinner
+          infinite
+          // isPlaying={true}
+          playDirection="forward"
+          visibleSlides={3}
+        >
+          <Slider>
+            <Slide index={0}><ImageWrapper><Image style={{ border: '1px solid gray', borderRadius: '2em' }} isBgImage src={photos[0].url}></Image></ImageWrapper></Slide>
+            <Slide index={1}><ImageWrapper><Image style={{ border: '1px solid gray', borderRadius: '2em' }} isBgImage src={photos[1].url}></Image></ImageWrapper></Slide>
+            <Slide index={2}><ImageWrapper><Image style={{ border: '1px solid gray', borderRadius: '2em' }} isBgImage src={photos[2].url}></Image></ImageWrapper></Slide>
+            <Slide index={3}><ImageWrapper><Image style={{ border: '1px solid gray', borderRadius: '2em' }} isBgImage src={photos[3].url}></Image></ImageWrapper></Slide>
+            <Slide index={4}><ImageWrapper><Image style={{ border: '1px solid gray', borderRadius: '2em' }} isBgImage src={photos[4].url}></Image></ImageWrapper></Slide>
+          </Slider>
+          <DotWrapper><DotGroup /></DotWrapper>
+        </CarouselProvider>
+      </Wrapper >
+    )
   }
 }
 export default Carousel;
