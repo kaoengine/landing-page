f305e5eaa0a7fcb5c14b711609d37cdc4b68b04d
Khanh <tran_khanh@outlook.com>
2020-04-22 20:57:33 +0700

    Update

---
+++ src/stories/3-Image.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ImageProduct from "../Components/Shared/Image"
+
+export default {
+    title: 'Image',
+};
+
+export const _Carousel = () => <ImageProduct></ImageProduct>
