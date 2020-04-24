bcaa56639a7ecd2780bf894ea525f9f4ce0507ca
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-04-24 20:12:19 +0700

    Added-TypeColor

--- src/Components/Shared/SectionVerbal/index.tsx
+++ src/Components/Shared/SectionVerbal/index.tsx
@@ -1,9 +1,10 @@
 import React, { FunctionComponent } from "react";
 import { Header } from "semantic-ui-react";
-
+import { Color } from "../../Shared/Badget";
 type SectionVerbalProps = {
   header: string;
   subHeader?: string;
+  Color?: Color;
 };
 
 const verbalStyle = {
@@ -14,9 +15,10 @@ const verbalStyle = {
 const SectionVerbal: FunctionComponent<SectionVerbalProps> = ({
   header,
   subHeader,
+  Color,
 }) => {
   return (
-    <Header as="h2">
+    <Header as="h2" color={Color}>
       {header}
       <Header.Subheader style={verbalStyle}>{subHeader}</Header.Subheader>
     </Header>
