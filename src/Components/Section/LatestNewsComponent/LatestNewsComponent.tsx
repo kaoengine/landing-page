a60841766d546526c34dcb98eff6e172a713b613
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-04-30 18:13:05 +0700

    U: Component LastestNewsComponent

--- src/Components/Section/LatestNewsComponent/LatestNewsComponent.tsx
+++ src/Components/Section/LatestNewsComponent/LatestNewsComponent.tsx
@@ -5,36 +5,44 @@ import { CardBlog } from "../../Shared/CardBlog";
 import { Color } from "../../Shared/Badget";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  @media only screen and (min-width: 320px) {
+    padding: 0 1em;
+    margin-bottom: 2.25em;
+  }
+`;
 class LatestNewsComponent extends React.Component {
   render() {
     return (
-      <Container id="news">
-        <Grid>
-          <Grid.Row centered>
-            <SectionVerbal
-              header={"Our Latest News"}
-              subHeader={
-                "Enthusiastically drive revolutionary opportunities before emerging leadership. Distinctively transform tactical methods of empowerment via resource sucking core."
-              }
-            />
-          </Grid.Row>
-          <Grid.Row colums={3}>
-            <Grid.Column computer={5}>
-              <CardBlog
-                title={"Lifestyles"}
-                color={Color.Blue}
-                date={Date().toString()}
-                comment={"45 comments"}
-                share={"10 shared"}
-                header={"Appropriately productize fully"}
-                description={
-                  "Some quick example text to build on the card title and make up the bulk."
+      <Wrapper>
+        <Container id="news">
+          <Grid stackable>
+            <Grid.Row centered>
+              <SectionVerbal
+                header={"Our Latest News"}
+                subHeader={
+                  "Enthusiastically drive revolutionary opportunities before emerging leadership. Distinctively transform tactical methods of empowerment via resource sucking core."
                 }
               />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
+            </Grid.Row>
+            <Grid.Row colums={3}>
+              <Grid.Column computer={5}>
+                <CardBlog
+                  title={"Lifestyles"}
+                  color={Color.Blue}
+                  date={Date().toString()}
+                  comment={"45 comments"}
+                  share={"10 shared"}
+                  header={"Appropriately productize fully"}
+                  description={
+                    "Some quick example text to build on the card title and make up the bulk."
+                  }
+                />
+              </Grid.Column>
+            </Grid.Row>
+          </Grid>
+        </Container>
+      </Wrapper>
     );
   }
 }
