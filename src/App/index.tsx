46774081799ff8110dc1848bcbc85cde20d62b1e
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2020-05-18 14:36:07 +0700

    U-Router

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -4,6 +4,7 @@ import Home, { homeComponents } from "../Page/Home";
 import SignInAndSignUp from "../Page/sign-in-and-sign-up/sign-in-and-sign-up";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import MenuContainer from "Components/Shared/Navbar/MenuContainer";
+import TestingRouter from "Components/TestingRouter";
 
 type iState = {
   visible: boolean;
@@ -23,6 +24,7 @@ class App extends React.Component<any, iState> {
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/signin" component={SignInAndSignUp} />
+          <Route exact path="/purchase" component={TestingRouter}></Route>
         </Switch>
       </Router>
     );
